d28533305e557ad3827c1d90b0009c5f8076b4f8
Pablo Grimao Company <Pablogrimaocompany@gmail.com>
2024-11-30 03:38:15 +0100

    Inicio3

--- VideoClub/Juego.php
+++ VideoClub/Juego.php
@@ -1,41 +1,34 @@
 <?php
 include_once "Soporte.php";
 
-class Dvd extends Soporte {
-    protected $idioma;              // Propiedad protegida, puede ser accesible desde clases hijas
-    private $formatPantalla;        // Propiedad privada
+class Juego extends Soporte {
+    protected $consola;
+    protected $minNumJugadores;
+    protected $maxNumJugadores;
 
-    // Constructor con tipos especificados para los parámetros
-    public function __construct(string $titulo, int $numero, float $precio, string $idioma, string $formatPantalla) {
+    // Constructor: inicializa las propiedades del juego
+    public function __construct(string $titulo, int $numero, float $precio, string $consola, int $minNumJugadores, int $maxNumJugadores) {
         parent::__construct($titulo, $numero, $precio); // Llamada al constructor de la clase padre
-        $this->idioma = $idioma;
-        $this->formatPantalla = $formatPantalla;
+        $this->consola = $consola;
+        $this->minNumJugadores = $minNumJugadores;
+        $this->maxNumJugadores = $maxNumJugadores;
     }
 
-    // Getter para idioma
-    public function getIdioma(): string {
-        return $this->idioma;
+    // Muestra los jugadores posibles para el juego
+    public function muestraJugadoresPosibles(): void {
+        if ($this->maxNumJugadores === 1) {
+            echo "Para un jugador.";
+        } elseif ($this->maxNumJugadores === 2) {
+            echo "Para dos jugadores.";
+        } else {
+            echo "Desde $this->minNumJugadores hasta $this->maxNumJugadores jugadores.";
+        }
     }
 
-    // Setter para idioma
-    public function setIdioma(string $idioma): void {
-        $this->idioma = $idioma;
-    }
-
-    // Getter para el formato de pantalla
-    public function getFormatPantalla(): string {
-        return $this->formatPantalla;
-    }
-
-    // Setter para el formato de pantalla
-    public function setFormatPantalla(string $formatPantalla): void {
-        $this->formatPantalla = $formatPantalla;
-    }
-    
-    // Método para mostrar un resumen de la clase
+    // Muestra el resumen del juego
     public function muestraResumen(): void {
-        // Llama al método muestraResumen() de la clase padre y agrega detalles específicos del DVD
-        echo parent::muestraResumen() . "<br>Idioma: $this->idioma, <br>Formato Pantalla: $this->formatPantalla";
+        // Llama al método muestraResumen() de la clase padre y añade más información específica del juego
+        echo parent::muestraResumen() . "<br>Consola: $this->consola, <br>Jugadores: Desde $this->minNumJugadores hasta $this->maxNumJugadores jugadores.<br>";
     }
 }
 ?>
